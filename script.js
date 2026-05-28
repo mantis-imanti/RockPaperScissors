6d7898257375ca3d93e7f78c370f59572c390fdc
mantis-imanti <mantis@imanti.edu.mx>
2026-05-27 20:54:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,148 +1,33 @@
-const URL =
-"https://teachablemachine.withgoogle.com/models/sBS7rUq_0/";
-
-
-let model, webcam, maxPredictions;
-
-
 async function init() {
 
-  const modelURL = URL + "model.json";
-  const metadataURL = URL + "metadata.json";
+  try {
 
-  model = await tmImage.load(
-    modelURL,
-    metadataURL
-  );
+    const stream =
+      await navigator.mediaDevices.getUserMedia({
+        video: true
+      });
 
-  maxPredictions = model.getTotalClasses();
+    const video =
+      document.createElement("video");
 
-  // Cámara
-  webcam = new tmImage.Webcam(
-    400,
-    400,
-    true
-  );
+    video.srcObject = stream;
 
-  await webcam.setup();
+    video.autoplay = true;
 
-  await webcam.play();
+    video.playsInline = true;
 
-  window.requestAnimationFrame(loop);
+    video.width = 400;
 
-  document
-    .getElementById("webcam-container")
-    .appendChild(webcam.canvas);
-}
+    document
+      .getElementById("webcam-container")
+      .appendChild(video);
 
+  } catch (err) {
 
-async function loop() {
+    console.error(err);
 
-  webcam.update();
-
-  await predict();
-
-  window.requestAnimationFrame(loop);
-}
-
-
-async function predict() {
-
-  const prediction =
-    await model.predict(webcam.canvas);
-
-  let highest = prediction[0];
-
-  prediction.forEach(p => {
-
-    if (p.probability > highest.probability) {
-      highest = p;
-    }
-
-  });
-
-  // Detectar jugada
-  if (highest.probability > 0.95) {
-
-    const playerMove =
-      highest.className;
-
-    document.getElementById(
-      "prediction"
-    ).innerHTML =
-      `Tú: ${emoji(playerMove)}`;
-
-    jugar(playerMove);
-
+    alert(
+      "No se pudo acceder a la cámara"
+    );
   }
-
-}
-
-
-function jugar(playerMove) {
-
-  const opciones = [
-    "Piedra",
-    "Papel",
-    "Tijeras"
-  ];
-
-  const computerMove =
-    opciones[
-      Math.floor(Math.random() * 3)
-    ];
-
-  document.getElementById(
-    "computer"
-  ).innerHTML =
-    `CPU: ${emoji(computerMove)}`;
-
-  let resultado = "";
-
-  if (playerMove === computerMove) {
-
-    resultado = "🤝 Empate";
-
-  } else if (
-
-    (playerMove === "Piedra" &&
-      computerMove === "Tijeras")
-
-    ||
-
-    (playerMove === "Papel" &&
-      computerMove === "Piedra")
-
-    ||
-
-    (playerMove === "Tijeras" &&
-      computerMove === "Papel")
-
-  ) {
-
-    resultado = "🏆 Ganaste";
-
-  } else {
-
-    resultado = "💀 Perdiste";
-  }
-
-  document.getElementById(
-    "result"
-  ).innerHTML = resultado;
-}
-
-
-function emoji(move) {
-
-  if (move === "Piedra")
-    return "✊";
-
-  if (move === "Papel")
-    return "✋";
-
-  if (move === "Tijeras")
-    return "✌️";
-
-  return move;
 }
